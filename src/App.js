a32f244e9c8ba1862b620090d1d20726611f8bc7
bradenschiller17 <bradenschiller17@gmail.com>
2019-02-15 16:49:53 -0600

    set up usage for clarifai api face detection & added keys.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import ImageLinkForm from './components/ImageLinkForm';
 import Rank from './components/Rank';
 // import FaceRecognition from './components/FaceRecognition';
 import Navigation from './components/Navigation';
+import Clarifai from 'clarifai';
+import { clarifai_api_key } from './keys';
+
+const app = new Clarifai.App({
+	apiKey: clarifai_api_key
+});
 class App extends Component {
 	constructor(props) {
 		super(props);
@@ -18,7 +24,14 @@ class App extends Component {
 	};
 
 	onSubmit = () => {
-		console.log('clicked');
+		app.models.predict('d94f289761a143ae80b9b42b892bd5f1', 'https://samples.clarifai.com/face-det.jpg').then(
+			function(response) {
+				console.log(response);
+			},
+			function(err) {
+				console.log(err);
+			}
+		);
 	};
 
 	render() {

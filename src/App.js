c7867ae10d4e42cd17e318e38ae3bd2c2731e6f4
bradenschiller17 <bradenschiller17@gmail.com>
2019-02-15 17:39:07 -0600

    receving face detection infromation based of photo upload & css fix

--- src/App.js
+++ src/App.js
@@ -29,9 +29,9 @@ class App extends Component {
 	onSubmit = () => {
 		this.setState({ imageUrl: this.state.input });
 		console.log(this.state.imageUrl);
-		app.models.predict(Clarifai.COLOR_MODEL, 'https://samples.clarifai.com/face-det.jpg').then(
+		app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input).then(
 			function(response) {
-				console.log(response);
+				console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
 			},
 			function(err) {
 				console.log(err);
--- src/components/FaceRecognition.js
+++ src/components/FaceRecognition.js
@@ -3,7 +3,7 @@ import React from 'react';
 const FaceRecognition = ({ imageUrl }) => {
 	return (
 		<div className="face-recognition-container">
-			<img className="face-recognition" src={imageUrl} alt="facial_recognition" />
+			<img className="face-recognition" src={imageUrl} alt="facial_recognition" width="700px" height="auto" />
 		</div>
 	);
 };
